eef8a5efe7db139032a51dc39546df1fdc4a82ed
Tirsan Sapkota <tirsanspkt159@gmail.com>
2025-05-26 17:12:05 -0400

    Added the basic structure of the classes we should implement

--- main.py
+++ main.py
@@ -3,3 +3,20 @@
 # From tkinter import *
 # import the random module
 
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
